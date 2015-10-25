264aec17efdf0a7b8cd60925d7de041320d70b56
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2015-10-25 08:32:40 +0100

    Fixed planetary layout to avoid moving the sectors when the text gets larger

--- Source/Flare/UI/Components/FlarePlanetaryBox.cpp
+++ Source/Flare/UI/Components/FlarePlanetaryBox.cpp
@@ -19,34 +19,34 @@ void SFlarePlanetaryBox::Construct(const SFlarePlanetaryBox::FArguments& InArgs)
 
 void SFlarePlanetaryBox::OnArrangeChildren(const FGeometry& AllottedGeometry, FArrangedChildren& ArrangedChildren) const
 {
-	if (Children.Num() > 1)
+	const FFlareStyleCatalog& Theme = FFlareStyleSet::GetDefaultTheme();
+
+	for (int32 ChildIndex = 0; ChildIndex < Children.Num(); ++ChildIndex)
 	{
-		FVector2D TypicalSize = Children[1].GetWidget()->GetDesiredSize();
+		// Get all required data
+		const SFlarePlanetaryBox::FSlot& CurChild = Children[ChildIndex];
+		const EVisibility ChildVisibility = CurChild.GetWidget()->GetVisibility();
+		FVector2D WidgetSize = CurChild.GetWidget()->GetDesiredSize();
+		FVector2D Offset = FVector2D::ZeroVector;
 
-		for (int32 ChildIndex = 0; ChildIndex < Children.Num(); ++ChildIndex)
+		// Child with index 0 is the main body, index 1 is probably the name but we don't care, others are sectors
+		if (ChildIndex > 0)
 		{
-			// Get all required data
-			const SFlarePlanetaryBox::FSlot& CurChild = Children[ChildIndex];
-			const EVisibility ChildVisibility = CurChild.GetWidget()->GetVisibility();
-			FVector2D Origin = (AllottedGeometry.GetLocalSize() - CurChild.GetWidget()->GetDesiredSize()) / 2;
-			FVector2D Offset = FVector2D::ZeroVector;
+			float X, Y;
+			float Angle = (360 / (Children.Num() - 1)) * (ChildIndex - 1) - 90;
+			FMath::PolarToCartesian(Radius, FMath::DegreesToRadians(Angle), X, Y);
+			Offset = FVector2D(X, Y);
 
-			// Child with index 0 is the main body, index 1 is probably the name but we don't care, others are sectors
-			if (ChildIndex > 0)
-			{
-				float X, Y;
-				float Angle = (360 / (Children.Num() - 1)) * (ChildIndex - 1) - 90;
-				FMath::PolarToCartesian(Radius, FMath::DegreesToRadians(Angle), X, Y);
-				Offset =  FVector2D(X, Y);
-			}
-			
-			// Arrange the child
-			ArrangedChildren.AddWidget(ChildVisibility, AllottedGeometry.MakeChild(
-				CurChild.GetWidget(),
-				Origin + Offset,
-				CurChild.GetWidget()->GetDesiredSize()
-			));
+			WidgetSize = FVector2D(CurChild.GetWidget()->GetDesiredSize().X, Theme.SectorButtonHeight);
 		}
+			
+		// Arrange the child
+		FVector2D Location = (AllottedGeometry.GetLocalSize() - WidgetSize) / 2 + Offset;
+		ArrangedChildren.AddWidget(ChildVisibility, AllottedGeometry.MakeChild(
+			CurChild.GetWidget(),
+			Location,
+			CurChild.GetWidget()->GetDesiredSize()
+		));
 	}
 }
 
