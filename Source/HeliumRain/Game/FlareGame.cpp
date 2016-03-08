9e96f69a036faa0ea17683f46e9fff347efe85e9
Frédéric Bertolus <fred.bertolus@gmail.com>
2016-03-08 21:11:15 +0100

    Fix scrap company payments

--- Source/HeliumRain/Game/FlareGame.cpp
+++ Source/HeliumRain/Game/FlareGame.cpp
@@ -272,6 +272,7 @@ void AFlareGame::Scrap(FName ShipImmatriculation, FName TargetStationImmatricula
 
 	if(ScrapingStation->GetCompany() != ShipToScrap->GetCompany())
 	{
+		ScrapingStation->GetCompany()->TakeMoney(ScrapRevenue);
 		ShipToScrap->GetCompany()->GiveMoney(ScrapRevenue);
 		GetPC()->Notify(LOCTEXT("ShipSellScrap", "Ship scrap complete"),
 			FText::Format(LOCTEXT("ShipSellScrapFormat", "Your ship {0} has been scraped for {1} credits!"), FText::FromString(ShipToScrap->GetImmatriculation().ToString()), FText::AsNumber(ScrapRevenue)),
