96e19cf3215e4066ca2e878372fc5cbabaf4a7ae
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2016-09-03 11:19:37 +0200

    Fixed bad text

--- Source/HeliumRain/Spacecrafts/Subsystems/FlareSpacecraftDamageSystem.cpp
+++ Source/HeliumRain/Spacecrafts/Subsystems/FlareSpacecraftDamageSystem.cpp
@@ -87,9 +87,9 @@ void UFlareSpacecraftDamageSystem::TickSystem(float DeltaSeconds)
 		else if (PC && LastDamageCauser && PC->GetCompany() == LastDamageCauser->GetParent()->GetCompany())
 		{
 			PC->Notify(LOCTEXT("ShipKilledCompany", "Target destroyed"),
-				FText::Format(LOCTEXT("ShipKilledCompanyFormat", "Your {0}-class ship destroyed a ship ({1}-class)"),
-					Spacecraft->GetParent()->GetDescription()->Name,
-					LastDamageCauser->GetParent()->GetDescription()->Name),
+				FText::Format(LOCTEXT("ShipKilledCompanyFormat", "Your {0}-class ship destroyed a ship ({1}-class)"), ,
+					LastDamageCauser->GetParent()->GetDescription()->Name
+					Spacecraft->GetParent()->GetDescription()->Name),
 				FName("ship-killed"),
 				EFlareNotification::NT_Info);
 		}
