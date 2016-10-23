3930be9d779e6d70514cbd53001033d00fa29606
Frédéric Bertolus <fred.bertolus@gmail.com>
2016-10-23 11:51:42 +0200

    Add helper to print economy status

--- Source/HeliumRain/Game/FlareGameTools.h
+++ Source/HeliumRain/Game/FlareGameTools.h
@@ -45,6 +45,9 @@ public:
 	UFUNCTION(exec)
 	void CheckEconomyBalance();
 
+	UFUNCTION(exec)
+	void PrintEconomyStatus();
+
 	/*----------------------------------------------------
 		World tools
 	----------------------------------------------------*/
