b749759fbb278903e771996653d32dd9ffda1c42
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2016-10-31 22:18:31 +0100

    Compromise value for cockpit lighting

--- Source/HeliumRain/Player/FlareCockpitManager.cpp
+++ Source/HeliumRain/Player/FlareCockpitManager.cpp
@@ -507,7 +507,7 @@ void AFlareCockpitManager::UpdatePower(float DeltaSeconds)
 	float PowerAlpha = CockpitPowerTime / CockpitPowerPeriod;
 
 	// Update lights
-	float Intensity = PowerAlpha * 10;
+	float Intensity = PowerAlpha * 50;
 	CockpitLight->SetIntensity(Intensity);
 	CockpitLight2->SetIntensity(Intensity);
 
