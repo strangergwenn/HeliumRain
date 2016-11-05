4f11a5b054f15674cc637ee32b0d44846bff3250
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2016-11-05 08:48:58 +0100

    Updated fighter cockpit

--- Source/HeliumRain/Player/FlarePlayerController.cpp
+++ Source/HeliumRain/Player/FlarePlayerController.cpp
@@ -288,7 +288,7 @@ void AFlarePlayerController::FlyShip(AFlareSpacecraft* Ship, bool PossessNow)
 	// Setup FOV
 	if (ShipPawn->GetParent()->IsMilitary())
 	{
-		PlayerCameraManager->SetFOV(92);
+		PlayerCameraManager->SetFOV(94);
 	}
 	else
 	{
