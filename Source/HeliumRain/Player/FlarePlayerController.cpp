188b00effe1a604cdc0cbfa951b3edd137a74044
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2017-03-23 20:50:09 +0100

    Reduce intensity of motion blur

--- Source/HeliumRain/Player/FlarePlayerController.cpp
+++ Source/HeliumRain/Player/FlarePlayerController.cpp
@@ -1831,7 +1831,7 @@ void AFlarePlayerController::SetUseMotionBlur(bool New)
 {
 	UseMotionBlur = New;
 	GetGame()->GetPostProcessVolume()->Settings.bOverride_MotionBlurAmount = true;
-	GetGame()->GetPostProcessVolume()->Settings.MotionBlurAmount = UseMotionBlur ? 0.5f : 0.0f;
+	GetGame()->GetPostProcessVolume()->Settings.MotionBlurAmount = UseMotionBlur ? 0.3f : 0.0f;
 }
 
 void AFlarePlayerController::SetPauseGameInMenus(bool New)
