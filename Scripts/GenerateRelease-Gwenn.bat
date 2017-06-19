e3baf4590336ac0145800b1f8d964b4dbad9ec69
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2017-06-19 20:25:45 +0200

    Update version

--- Scripts/GenerateRelease-Gwenn.bat
+++ Scripts/GenerateRelease-Gwenn.bat
@@ -4,7 +4,7 @@ set OutputDir=D:\HR-Builds
 set ProjectDir=D:\HeliumRain
 set EngineDir=D:\Dock\UE4_Packaged\UE_4.16
 
-set VersionName=beta-120617
+set VersionName=beta-190617
 if not "%1"=="" (
 	set VersionName=%1
 )
