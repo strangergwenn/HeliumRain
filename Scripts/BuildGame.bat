0eab576026de6baa9b87dd7b56093c07bdc4273e
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2017-07-02 10:57:47 +0200

    Upgrade build script to include en-EN, fr-FR cultures

--- Scripts/BuildGame.bat
+++ Scripts/BuildGame.bat
@@ -22,4 +22,4 @@ rem =======================================================
 rem Run UAT
 rem =======================================================
 
-%UAT% -ScriptsForProject=%InputProject% BuildCookRun -nocompile -nocompileeditor -installed -nop4 -project=%InputProject% -cook -stage -archive -archivedirectory=%OutputDir% -package -clientconfig=%BuildMode% -ue4exe=UE4Editor-Cmd.exe -clean -pak -compressed -prereqs -distribution -nodebuginfo -createreleaseversion=%VersionName% -targetplatform=%Platform% -build -utf8output
+%UAT% -ScriptsForProject=%InputProject% BuildCookRun -nocompile -nocompileeditor -installed -nop4 -project=%InputProject% -cook -stage -archive -archivedirectory=%OutputDir% -package -clientconfig=%BuildMode% -ue4exe=UE4Editor-Cmd.exe -clean -pak -compressed -prereqs -distribution -nodebuginfo -createreleaseversion=%VersionName% -targetplatform=%Platform% -build -utf8output -CookCultures=en-US+fr-FR
