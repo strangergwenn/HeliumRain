80a03713c020f97f07874a4fc583f76971936c50
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2017-07-11 21:57:09 +0200

    Cleanup upload script

--- Scripts/2-Upload.py
+++ Scripts/2-Upload.py
@@ -14,13 +14,13 @@ steamPassword = steamConfig["password"]
 steamData.close()
 
 # Generate paths
-relativeAppFile = os.path.join("..", "scripts", steamAppFile)
+buildAppFile = os.path.join("..", steamAppFile)
 buildRootDir = os.path.join(steamSDKDir, "tools", "ContentBuilder")
 
 # Generate full command line
 commandLine = os.path.join(steamBuilder, steamCommand)
 commandLine += " +login " + steamUser + " " + steamPassword
-commandLine += " +run_app_build " + relativeAppFile
+commandLine += " +run_app_build " + buildAppFile
 commandLine += " +quit"
 
 # Call
