6851b01c38fe0c46a7a225eb2dcb4108e908d4be
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2017-09-05 17:47:51 +0200

    Fix crash when loading UE editor with custom UI scaling

--- Source/HeliumRain/UI/Style/FlareScalingRule.cpp
+++ Source/HeliumRain/UI/Style/FlareScalingRule.cpp
@@ -5,8 +5,14 @@ float UFlareScalingRule::GetDPIScaleBasedOnSize(FIntPoint Size) const
 {
 	float NominalAspectRatio = (16.0f / 9.0f);
 
+	// Loading game
+	if (Size.X == 0 || Size.Y == 0)
+	{
+		return 1;
+	}
+
 	// Wide screen : scale 1 at 1920, scale 2 at 3840... 
-	if (Size.X / Size.Y > NominalAspectRatio)
+	else if (Size.X / Size.Y > NominalAspectRatio)
 	{
 		return (Size.Y / 1080.0f);
 	}
