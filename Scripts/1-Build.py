fdca65f1e564477d5a84e3362317b6905ca860e4
Gwennaël Arbona <gwennael.arbona@gmail.com>
2019-01-14 23:43:16 +0100

    Make modding optional

--- Scripts/1-Build.py
+++ Scripts/1-Build.py
@@ -66,13 +66,14 @@ for platform in buildPlatforms:
 	os.system(commandLine)
 	
 	# Copy Boiler files and other tools
-	if platform == 'Linux':
-		buildOutputDir = outputDir + "/LinuxNoEditor"
-		shutil.copyfile("../HeliumRainLauncher", buildOutputDir + "/HeliumRainLauncher")
-		shutil.copyfile("../steam_appid.txt", buildOutputDir + "/steam_appid.txt")
-		shutil.copytree("../Icons", buildOutputDir + "/Icons")
-	else:
-		buildOutputDir = outputDir + "/WindowsNoEditor"
-		shutil.copyfile("../HeliumRainLauncher.exe", buildOutputDir + "/HeliumRainLauncher.exe")
-		shutil.copyfile("../steam_api64.dll", buildOutputDir + "/steam_api64.dll")
-		shutil.copyfile("../steam_appid.txt", buildOutputDir + "/steam_appid.txt")
+	if projectConfig["modding"]:
+		if platform == 'Linux':
+			buildOutputDir = outputDir + "/LinuxNoEditor"
+			shutil.copyfile("../HeliumRainLauncher", buildOutputDir + "/HeliumRainLauncher")
+			shutil.copyfile("../steam_appid.txt", buildOutputDir + "/steam_appid.txt")
+			shutil.copytree("../Icons", buildOutputDir + "/Icons")
+		else:
+			buildOutputDir = outputDir + "/WindowsNoEditor"
+			shutil.copyfile("../HeliumRainLauncher.exe", buildOutputDir + "/HeliumRainLauncher.exe")
+			shutil.copyfile("../steam_api64.dll", buildOutputDir + "/steam_api64.dll")
+			shutil.copyfile("../steam_appid.txt", buildOutputDir + "/steam_appid.txt")
